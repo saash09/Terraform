5971f06acb65e8ce3c0d7a5616e911f7ff3ed1ea
saash09 <akshatharao92@gmail.com>
2024-09-12 15:25:32 +0530

    Update VMResoruces.tf

--- VMResoruces.tf
+++ VMResoruces.tf
@@ -1,11 +1,11 @@
 resource "azurerm_virtual_machine" "vm1" {
-  name                  = "akkitestvm"
+  name                  = "akkitestvm1"
   resource_group_name   = "TerraformRG"
   location              = "East US"
   network_interface_ids = [azurerm_network_interface.nic1.id]
   vm_size               = "Standard_DS1_v2"
   storage_os_disk {
-    name = "araodisk"
+    name = "araodisk1"
     caching = "ReadWrite"
     managed_disk_type = "Standard_LRS"
     create_option = "FromImage"
@@ -21,7 +21,7 @@ resource "azurerm_virtual_machine" "vm1" {
     version = "latest"
   }
   os_profile {
-    computer_name  = "trainingsystem1"
+    computer_name  = "arao1"
     admin_username = "trainingadmin"
     admin_password = "Accenture123$$"
   }
@@ -37,7 +37,7 @@ resource "azurerm_virtual_machine" "vm1" {
 }
 
 resource "azurerm_virtual_network" "vnet1" {
-  name                = "testvnet"
+  name                = "araovnet"
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
   resource_group_name = "TerraformRG"
@@ -45,19 +45,19 @@ resource "azurerm_virtual_network" "vnet1" {
 
 
 resource "azurerm_subnet" "subnet1" {
-  name                 = "testarao"
+  name                 = "araosubnet"
   resource_group_name  = "TerraformRG"
   virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.3.0/24"]
 }
 
 resource "azurerm_network_interface" "nic1" {
-  name                = "araonic"
+  name                = "araonic1"
   location            = "East US"
   resource_group_name = "TerraformRG"
 
   ip_configuration {
-    name                          = "arao1"
+    name                          = "araoip1"
     subnet_id                     = azurerm_subnet.subnet1.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id =azurerm_public_ip.public1.id
@@ -65,7 +65,7 @@ resource "azurerm_network_interface" "nic1" {
 }
 
 resource "azurerm_public_ip" "public1" {
-  name = "arao-ip"
+  name = "arao-ip01"
   resource_group_name = "TerraformRG"
   location = "East US"
   allocation_method = "Static"
